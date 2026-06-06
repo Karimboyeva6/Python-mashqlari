25ca5a39befb78541e207af1cf77abe8b52a2a9b
Karimboyeva6 <ykarimboyeva6@gmail.com>
2026-06-06 06:32:12 +0500

    Create Mashq3.py

---
+++ Mashq3.py
@@ -0,0 +1,16 @@
+print(5**4)#625
+print(22%4)#2
+tomon = 125
+yuza = tomon ** 2
+perimetr= 4 * tomon
+print('kvadrat yuzi:'yuz)#15625
+print('kvadrat perimetri:'perimetr)#500diametr = 12
+radius = diametr / 2
+pi = 3.14
+doira_yuz = pi * radius ** 2
+print("Doira yuzi:", doira_yuz)       # 113.04
+# 5. To'g'ri burchakli uchburchak gipotenuzasi
+a = 6
+b = 7
+gipotenuza = math.sqrt(a ** 2 + b ** 2)
+print("Gipotenuza:", round(gipotenuza, 2))  # 9.22
