b0dd9e9ad742921f9b9beb8eb05b78a2dea6d68c
Karimboyeva6 <ykarimboyeva6@gmail.com>
2026-06-07 17:06:54 +0500

    Add basic Python script for greetings and circle area

---
+++ Mashq4.py
@@ -0,0 +1,12 @@
+x = ("Hello World!")
+print(x)#Hello World!
+xabar = ('Salom dòstim')
+print(xabar)#Salom dòstim
+xabar = ('Xayr dòstim')
+print(xabar)#Xayr dòstim
+class = ("Salom") 
+print(class)#xato kalit sozdan foydalanb bolmaydi
+radius = 5
+pi = 3.14159
+aylana_yuzi = pi * radius**2
+print("Radiusi" , radius, "ga teng aylananing yuzi=", aylana_yuzi)#78.53975
