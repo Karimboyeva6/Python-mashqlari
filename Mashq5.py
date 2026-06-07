bc0e9a157a6f2ec861cea42ae65ad33fe591ab50
Karimboyeva6 <ykarimboyeva6@gmail.com>
2026-06-07 22:41:04 +0500

    Create Mashq5.py

---
+++ Mashq5.py
@@ -0,0 +1,28 @@
+# 1-2 mashq
+kocha = "Bog'bon"
+mahalla = "Sog'bon"
+tuman = "Bodomzor"
+viloyat = "Samarqand"
+print(f"{kocha} ko'chasi, {mahalla} mahallasi, {tuman} tumani, {viloyat} viloyati")
+#Bog'bon ko'chasi, Sog'bon mahallasi, Bodomzor tumani, Samarqand viloyati  
+# 3-mashq
+kocha = input("Ko'chasi: ")#Bog'bon
+mahalla = input("Mahallasi: ")#Sog'bon
+tuman = input("Tumani: ")#Bodomzor
+viloyat = input("Viloyati: ")#Samarqand
+print(f"{kocha} ko'chasi, {mahalla} mahallasi, {tuman} tumani, {viloyat} viloyati")
+#Bog'bon ko'chasi, Sog'bon mahallasi, Bodomzor tumani, Samarqand viloyati  
+# 4-mashq - yangi qator \n
+print(f"{kocha} ko'chasi,\n{mahalla} mahallasi,\n{tuman} tumani,\n{viloyat} viloyati")
+#Bog'bon ko'chasi
+Sog'bon mahallasi
+Bodomzor tumani
+Samarqand viloyati
+# 5-mashq - manzil o'zgaruvchisi
+manzil = f"{kocha} ko'chasi,\n{mahalla} mahallasi,\n{tuman} tumani,\n{viloyat} viloyati"
+
+# 6-mashq - metodlar
+print(manzil.title())
+print(manzil.upper())
+print(manzil.lower())
+print(manzil.capitalize())
