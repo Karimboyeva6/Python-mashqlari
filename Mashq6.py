a7c794f434d39dd158bec8fe4a3d36458bcb5e48
Karimboyeva6 <ykarimboyeva6@gmail.com>
2026-06-10 08:11:34 +0500

    Add basic arithmetic functions in Mashq6.py

---
+++ Mashq6.py
@@ -0,0 +1,16 @@
+def qoshish(x, y):
+    return x + y
+
+def ayirish(x, y):
+    return x - y
+
+def kopaytirish(x, y):
+    return x * y
+
+def bolish(x, y):
+    return x / y
+
+print(qoshish(5, 6))    # 11
+print(ayirish(4, 3))    # 1
+print(kopaytirish(8, 9)) # 72
+print(bolish(4, 2))     # 2.0
